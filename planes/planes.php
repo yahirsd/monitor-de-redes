4c8dd1f83d5934340b101964e720da2982cb8327
yahirsd <dysd.106mt@gmail.com>
2024-02-09 20:23:16 -0600

    creacion de la carpeta styles asi como su archivo de estilos

--- planes/planes.php
+++ planes/planes.php
@@ -1,66 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Planes</title>
-    </head>
-        <style>
-        body {
-          font-family: Arial, sans-serif;
-          margin: 0;
-          padding: 0;
-        }
-    
-        nav {
-          background-color: #333;
-        }
-    
-        nav ul {
-          list-style-type: none;
-          margin: 0;
-          padding: 0;
-          text-align: center; /* Centra los elementos */
-        }
-    
-        nav ul li {
-          display: inline-block; /* Muestra los elementos en línea horizontal */
-        }
-    
-        nav ul li a {
-          display: block;
-          color: white;
-          text-decoration: none;
-          padding: 15px 20px;
-        }
-    
-        nav ul li a:hover {
-          background-color:#555;
-        }
-    
-        .content {
-          padding: 20px;
-        }
-      </style>
-    <body>
-        <nav>
-            <ul>
-                <li><a href="#network">Localizar Dispositivos</a></li>
-                <li><a href="#Inventario">Inventario</a></li>
-                <li><a href="#Prevención">Planes de Prevención</a></li>
-                <li><a href="#Fallas">Seguimiento de Fallas</a></li>
-                <li><a href="#Configuraciones">Configuraciones</a></li>
-            </ul>
-        </nav>
 
-        <div class="content">
-            <h1>planes de prevencion</h1>   
-            <!-- Contenido de la página -->
-            <div class="cajita">
-                <div id="item1" class="items">
-                    <img src="" alt="">
-                </div>
-            </div>
-        </div>
-    </body>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../styles/main.css">
+  <title>Planes</title>
+</head>
+
+<body>
+  <nav>
+    <ul>
+      <li><a href="#network">Localizar Dispositivos</a></li>
+      <li><a href="#Inventario">Inventario</a></li>
+      <li><a href="#Prevención">Planes de Prevención</a></li>
+      <li><a href="#Fallas">Seguimiento de Fallas</a></li>
+      <li><a href="#Configuraciones">Configuraciones</a></li>
+    </ul>
+  </nav>
+
+  <div class="content">
+    <h1>planes de prevencion</h1>
+    <!-- Contenido de la página -->
+    <div class="cajita">
+      <div id="item1" class="items">
+        <img src="" alt="">
+      </div>
+    </div>
+  </div>
+</body>
+
 </html>
