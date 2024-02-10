aed7d8ed7b5eb5f6d16979d669d316d34c0964a8
yahirsd <dysd.106mt@gmail.com>
2024-02-09 20:15:50 -0600

    solucion de error en la etiqueta body

--- planes/planes.php
+++ planes/planes.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Planes</title>
     </head>
-    <body>
         <style>
         body {
           font-family: Arial, sans-serif;
