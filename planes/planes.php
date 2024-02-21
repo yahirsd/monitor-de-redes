beab447a56a0e9642307a3f47b09c4899b8e80cc
yahirsd <dysd.106mt@gmail.com>
2024-02-21 15:37:07 -0600

    correccion en los links de la nav bar planes

--- planes/planes.php
+++ planes/planes.php
@@ -15,11 +15,12 @@
   <header>
     <nav>
       <ul>
-        <li><a href="#network">Localizar Dispositivos</a></li>
-        <li><a href="../Redes-inventarios/inventarioG.html">Inventario</a></li>
-        <li><a href="#Prevención">Planes de Prevención</a></li>
-        <li><a href="../seguimiento_Fallas/Fallas_main.html">Seguimiento de Fallas</a></li>
-        <li><a href="#Configuraciones">Configuraciones</a></li>
+        <li><a href="../index.html">Inicio</a></li>
+        <li><a href="../Localizacion-Dispositivos/">Localizar Dispositivos</a></li>
+        <li><a href="Redes-Inventario/inventarioG.html">Inventario</a></li>
+        <li><a href="../planes/planes.php">Planes de Prevención</a></li>
+        <li><a href="../seguimiento_Fallas/Fallas_main.html">Seguimiento de fallas</a></li>
+
       </ul>
     </nav>
     <h1>Planes de prevencion</h1>
