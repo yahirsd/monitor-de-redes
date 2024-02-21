f76a9413bdf2e90dd3f4fd6fe2a9963ab989f6f6
yahirsd <dysd.106mt@gmail.com>
2024-02-21 14:16:18 -0600

    conexion de la base de datos a planes

--- planes/planes.php
+++ planes/planes.php
@@ -10,8 +10,18 @@
 </head>
 
 <body>
+
+  <?php
+  include("../db/conection.php")
+  ?>
+  <div id="myModal" class="modal">
+    <div class="modal-content">
+      <span class="close">&times;</span>
+      <p>Estas seguro que deseas borrar el archivo</p>
+      <button>OK</button>
+    </div>
   </div>
-  </div>
+
   <header>
     <nav>
       <ul>
@@ -28,11 +38,11 @@
   <main class="container">
 
     <aside class="aside">
-      <form class="form">
+      <form enctype="multipart/form-data" method="post" class="form">
         <legend class=form__legend>Agregar un nuevo registro</legend>
         <div class="field">
           <label for="input-name" class="ha-screen-reader">Nombre</label>
-          <input id="input-name" class="field__input" placeholder="Archivo Falla">
+          <input id="input-name" class="field__input" name="name" placeholder="Archivo Falla">
           <span class="field__label-wrap" aria-hidden="true">
             <span class="field__label">Nombre</span>
           </span>
@@ -40,7 +50,7 @@
 
         <div class="field">
           <label for="input-description" class="ha-screen-reader">Descripcion</label>
-          <input id="input-description" class="field__input" placeholder="Falla ejemplo">
+          <input id="input-description" class="field__input" name="description" placeholder="Falla ejemplo">
           <span class="field__label-wrap" aria-hidden="true">
             <span class="field__label">Descripcion</span>
           </span>
@@ -84,11 +94,6 @@
             <td>Este es un ejemplo de una escripcion</td>
             <td>20/20/20</td>
             <td><button id="openModalBtn">Eliminar</button>
-
-              <div id="myModal" class="modal">
-                <div class="modal-content">
-                  <span class="close">&times;</span>
-                  <p>Estas seguro que deseas borrar el archivo</p>
             </td>
           </tr>
 
@@ -121,3 +126,50 @@
 <script src="busqueda.js"></script>
 
 </html>
+<?php
+if (isset($_POST["name"]) and isset($_POST["description"])) {
+  $nombre = $_POST["name"];
+  $descripcion = $_POST["description"];
+  $ruta = "file";
+
+  // Consulta de inserción
+  $sql = "INSERT INTO planes (nombre, descripcion, ruta) VALUES (?, ?, ?)";
+
+  if ($stmt = $conexion->prepare($sql)) {
+    $stmt->bind_param("sss", $nombre, $descripcion, $ruta);
+    if ($stmt->execute()) {
+      upLoadFile();
+      echo "Inserción exitosa en la tabla 'planes'.";
+    } else {
+      echo "Error al ejecutar la consulta: " . $stmt->error;
+    }
+
+    $stmt->close();
+  } else {
+    echo "Error al preparar la consulta: " . $conexion->error;
+  }
+
+  // Cierra la conexión al finalizar
+  $conexion->close();
+}
+
+
+
+function upLoadFile()
+{
+  $dir_subida = 'files/';
+  $fichero_subido = $dir_subida . basename($_FILES['file']['name']);
+
+  echo '<pre>';
+  if (move_uploaded_file($_FILES['file']['tmp_name'], $fichero_subido)) {
+    echo "El fichero es válido y se subió con éxito.\n";
+  } else {
+    echo "¡Posible ataque de subida de ficheros!\n";
+  }
+
+  echo 'Más información de depuración:';
+  print_r($_FILES);
+
+  print "</pre>";
+}
+?>
